52dd9ce0dc7603657368fe16c2aa568c7077b8d4
legrank <flyu.gyu@yandex.ru>
2022-11-28 17:13:54 +0300

    webpack

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-import './index.css'
-import MY_IMAGE from './assets/i.webp'
-
 console.log('Hello World!')
-
-const h1 = document.createElement('h1')
-h1.textContent = 'I love JavaScript'
-h1.classList.add('h1')
-h1.classList.add('center')
-document.body.append(h1)
-const img = document.createElement('img')
-img.src = MY_IMAGE
-img.classList.add('center')
-document.body.append(img)
